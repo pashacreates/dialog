162d98cfb34c07de24fc18d4ce8c4abd375bbade
PavelShakuro <sh.pasha.v@gmail.com>
2020-03-31 16:00:29 +0300

    Ready

--- script.js
+++ script.js
@@ -1,133 +1,187 @@
 dialogs = [{
-    avatar: 'img/ava-1.png',
-    dialogName: 'John',
+    avatar: 'img/fil.png',
+    dialogName: 'Филипп',
     messages: [{
-        date: new Date(2020, 02, 07, 10, 0, 0),
-        name: 'John',
-        text: 'Hello!'
-    }, {
-        date: new Date(2020, 02, 07, 10, 1, 11),
+        date: new Date(2011, 04, 12, 15, 1, 55),
         name: 'You',
-        text: 'Hello!'
+        text: 'У чувака пошла кровь из носа и он просит тридцатку?'
     }, {
-        date: new Date(2020, 02, 07, 10, 1, 55),
-        name: 'John',
-        text: 'How are you?'
+        date: new Date(2011, 04, 12, 15, 2, 0),
+        name: 'Филипп',
+        text: 'Скажите, Дрисс, как вы думаете, почему людей тянет к искусству?'
     }, {
-        date: new Date(2020, 02, 07, 10, 2, 05),
+        date: new Date(2011, 04, 12, 15, 2, 05),
         name: 'You',
-        text: 'Fine, thank you'
+        text: 'Может, потому что это прибыльно?'
+    }, {
+        date: new Date(2011, 04, 12, 15, 2, 10),
+        name: 'Филипп',
+        text: 'Нет, это единственный способ оставить след на земле.'
+    }, {
+        date: new Date(2011, 04, 12, 15, 2, 12),
+        name: 'You',
+        text: 'Фигня это, Филипп! Я сейчас куплю красок на полтинник и наслежу на полную катушку. Ещё синий добавлю бесплатно.'
+    }, {
+        date: new Date(2011, 04, 12, 15, 2, 15),
+        name: 'Филипп',
+        text: 'Так, хватит болтать чепуху. Дайте конфету.'
+    }, {
+        date: new Date(2011, 04, 12, 15, 2, 18),
+        name: 'You',
+        text: 'Не-а'
+    }, {
+        date: new Date(2011, 04, 12, 15, 2, 20),
+        name: 'Филипп',
+        text: 'Дайте мне конфету.'
+    }, {
+        date: new Date(2011, 04, 12, 15, 2, 23),
+        name: 'You',
+        text: 'Нету ручек - нет конфетки!'
     }]
 }, {
-    avatar: 'img/ava-3.png',
-    dialogName: 'Marry',
-    messages: [{
-        date: new Date(2020, 02, 12, 22, 1, 55),
-        name: 'Marry',
-        text: 'Hi!'
-    }, {
-        date: new Date(2020, 02, 12, 22, 2, 0),
-        name: 'You',
-        text: 'Hello!'
-    }, {
-        date: new Date(2020, 02, 12, 22, 2, 55),
-        name: 'Marry',
-        text: 'How r u?'
-    }, {
-        date: new Date(2020, 02, 12, 22, 2, 0),
-        name: 'You',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi soluta earum necessitatibus voluptate esse molestias quibusdam? Beatae necessitatibus et cum veniam. Alias, incidunt ipsam perferendis quas omnis corrupti minima unde!'
-    }, {
-        date: new Date(2020, 02, 12, 22, 2, 55),
-        name: 'Marry',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi soluta earum necessitatibus voluptate esse molestias Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi soluta earum necessitatibus voluptate esse molestiasLorem ipsum dolor sit amet consectetur adipisicing elit. Sequi soluta earum necessitatibus voluptate esse molestias'
-    }]
-}, {
-    avatar: 'img/ava-2.png',
+    avatar: 'img/vin.png',
     dialogName: 'Винсент',
     messages: [{
-        date: new Date(2020, 02, 14, 12, 3, 11),
+        date: new Date(1994, 09, 14, 12, 3, 11),
         name: 'Винсент',
         text: ' А знаешь, как в Париже называют четвертьфунтовый чизбургер?'
     }, {
-        date: new Date(2020, 02, 14, 12, 3, 45),
+        date: new Date(1994, 09, 14, 12, 3, 45),
         name: 'You',
         text: ' Что, они не зовут его четвертьфунтовый чизбургер?'
     }, {
-        date: new Date(2020, 02, 14, 12, 3, 45),
+        date: new Date(1994, 09, 14, 12, 3, 45),
         name: 'Винсент',
         text: 'У них там метрическая система. Они вообще там не понимают, что за хрен четверть фунта.'
     }, {
-        date: new Date(2020, 02, 14, 12, 3, 45),
+        date: new Date(1994, 09, 14, 12, 3, 45),
         name: 'You',
         text: 'И как они его зовут?'
     }, {
-        date: new Date(2020, 02, 14, 12, 3, 45),
+        date: new Date(1994, 09, 14, 12, 3, 45),
         name: 'Винсент',
         text: 'Они зовут его «Роял чизбургер».'
     }, {
-        date: new Date(2020, 02, 14, 12, 3, 45),
+        date: new Date(1994, 09, 14, 12, 3, 45),
         name: 'You',
         text: '«Роял чизбургер»? А как же тогда они зовут «Биг Мак»?',
     }, {
-        date: new Date(2020, 02, 14, 12, 3, 45),
+        date: new Date(1994, 09, 14, 12, 3, 45),
         name: 'Винсент',
         text: '«Биг Мак» это «Биг Мак», только они называют его «Лё Биг Мак».',
     }, {
-        date: new Date(2020, 02, 14, 12, 3, 45),
+        date: new Date(1994, 09, 14, 12, 3, 45),
         name: 'Винсент',
         text: 'А знаешь что они там в Голландии льют на картошку фри вместо кетчупа?',
     }, {
-        date: new Date(2020, 02, 14, 12, 3, 45),
+        date: new Date(1994, 09, 14, 12, 3, 45),
         name: 'You',
         text: 'Что?'
     }, {
-        date: new Date(2020, 02, 14, 12, 3, 45),
+        date: new Date(1994, 09, 14, 12, 3, 45),
         name: 'Винсент',
         text: 'Майонез!'
     }, {
-        date: new Date(2020, 02, 14, 12, 3, 45),
+        date: new Date(1994, 09, 14, 12, 3, 45),
         name: 'You',
         text: 'Черт подери!'
     }, {
-        date: new Date(2020, 02, 14, 12, 3, 45),
+        date: new Date(1994, 09, 14, 12, 3, 45),
         name: 'Винсент',
         text: 'Я сам видел, как они это делают. Просто заливают все этим дерьмом.'
     }, {
-        date: new Date(2020, 02, 14, 12, 3, 45),
+        date: new Date(1994, 09, 14, 12, 3, 45),
         name: 'You',
         text: 'Гамбургеры! Краеугольный камень здорового питания.'
     }]
 }, {
-    avatar: 'img/ava-3.png',
+    avatar: 'img/vito.png',
     dialogName: 'Вито Корлеоне',
     messages: [{
-        date: new Date(2020, 02, 12, 22, 1, 55),
+        date: new Date(1972, 03, 14, 13, 5, 55),
         name: 'Вито Корлеоне',
         text: 'Я знаю тебя много лет, но ты никогда не обращался ко мне за советом или помощью. Я не могу вспомнить, когда ты в последний раз приглашал меня в свой дом на чашку кофе, хотя моя жена — крёстная твоего единственного ребёнка. Будем сейчас откровенны: ты никогда не искал моей дружбы и ты боялся быть у меня в долгу.'
     }, {
-        date: new Date(2020, 02, 12, 22, 2, 0),
+        date: new Date(1972, 03, 14, 13, 6, 0),
         name: 'You',
         text: ' Я не хотел нажить неприятностей.'
     }, {
-        date: new Date(2020, 02, 12, 22, 2, 55),
+        date: new Date(1972, 03, 14, 13, 6, 5),
         name: 'Вито Корлеоне',
         text: 'Я понимаю. Ты нашёл в Америке рай: у тебя хорошо шёл бизнес, тебя защищала полиция и тебе не нужны были такие друзья, как я. А теперь ты приходишь и говоришь: Дон Корлеоне, мне нужна справедливость. Но ты не просишь с уважением, не предлагаешь дружбу, даже не думаешь обратиться ко мне — крёстный. Нет, ты приходишь ко мне в дом в день свадьбы моей дочери и просишь убивать за деньги.'
     }]
-}]
+}, {
+    avatar: 'img/merlin.png',
+    dialogName: 'Мэрлин',
+    messages: [{
+        date: new Date(2000, 02, 12, 22, 1, 55),
+        name: 'You',
+        text: 'А как переводится «How are you»?'
+    }, {
+        date: new Date(2000, 02, 12, 22, 2, 0),
+        name: 'Мэрлин',
+        text: '«Как дела или как поживаешь».'
+    }, {
+        date: new Date(2000, 02, 12, 22, 2, 04),
+        name: 'You',
+        text: 'И что, всем интересно, как у меня дела?'
+    }, {
+        date: new Date(2000, 02, 12, 22, 2, 8),
+        name: 'Мэрлин',
+        text: 'Не-а, неинтересно.'
+    }, {
+        date: new Date(2000, 02, 12, 22, 2, 13),
+        name: 'You',
+        text: 'А чего тогда спрашивают?'
+    }, {
+        date: new Date(2000, 02, 12, 22, 2, 18),
+        name: 'Мэрлин',
+        text: 'Просто так. Здесь вообще всё просто так, кроме денег.'
+    }]
+}, {
+    avatar: 'img/косой.png',
+    dialogName: 'Косой',
+    messages: [{
+        date: new Date(1971, 12, 13, 12, 1, 55),
+        name: 'You',
+        text: 'Сидит?'
+    }, {
+        date: new Date(1971, 12, 13, 12, 2, 01),
+        name: 'Косой',
+        text: 'Кто?'
+    }, {
+        date: new Date(1971, 12, 13, 12, 2, 12),
+        name: 'You',
+        text: 'Ну, мужик этот твой!'
+    }, {
+        date: new Date(1971, 12, 13, 12, 2, 23),
+        name: 'Косой',
+        text: 'Хе-хе-хе! Во деревня, а! Ну ты даешь! Кто же его посадит? Он же памятник!'
+    }]
+}, {
+    avatar: 'img/ava-2.png',
+    dialogName: 'John',
+    messages: []
+}, {
+    avatar: 'img/ava-3.png',
+    dialogName: 'Marry',
+    messages: []
+}, ]
 
 const dialogsList = document.getElementById('dialogsList');
 const dialog = document.getElementById('dialog');
 const search = document.getElementById('search');
 
-search.oninput = function() {
-    let val = this.value.trim();
+function searchMessage() {
+    let val = this.value.trim().toLowerCase();
 
     if (val != '') {
         for (let i = 0; i < dialogs.length; i++) {
             //этим циклом из списка диалогов прячем диалоги не соответствующие критерию поиска
+            if (dialogs[i].messages.length == 0) { dialogs[i].display = 'hidden'; }
             for (let j = 0; j < dialogs[i].messages.length; j++) {
-                if (dialogs[i].messages[j].text.search(val) == -1) {
+
+                if (dialogs[i].messages[j].text.toLowerCase().search(val) == -1) {
                     dialogs[i].display = 'hidden';
                 } else {
                     dialogs[i].display = 'display';
@@ -137,7 +191,7 @@ search.oninput = function() {
             //пришлось сделать отдельный цикл без break
             //для перезаписи последнего сообщения в списке диалогов
             for (let j = 0; j < dialogs[i].messages.length; j++) {
-                if (dialogs[i].messages[j].text.search(val) == -1) {
+                if (dialogs[i].messages[j].text.toLowerCase().search(val) == -1) {
                     dialogs[i].messages[j].mark = 0;
                 } else {
                     dialogs[i].messages[j].mark = 1;
@@ -145,30 +199,51 @@ search.oninput = function() {
             }
         }
     } else {
+
         dialogs.forEach(function(elem) {
+            if (elem.messages.length == 0) {
+                elem.display = 'display';
+            }
             elem.messages.forEach(function(m) {
                 elem.display = 'display';
-                m.mark = 1;
+                m.mark = 0;
             })
         });
     }
     renderDialogsList();
+    dialog.innerText = '';
 }
 
-function renderDialog(a, index) {
+search.addEventListener('input', searchMessage);
+
+
+function renderDialog(a) {
     dialog.innerText = '';
     a.messages.forEach(item => {
         let message = 'not-my';
-        if (item.name === 'You') { message = 'my' };
+        let ava = a.avatar;
+        if (item.name === 'You') {
+            message = 'my';
+            ava = 'img/userAva.png';
+        };
+        if (item.mark == 1) { message += ' mark' };
         dialog.insertAdjacentHTML('beforeend', `
-            <li class='message ${message}'>
-                <div class=''>
+            <li class='${message}'>
+                <img src='${ava}'>
+                <div>
                     <p>${item.name}:</p>
                     <p>${item.text}</p>
+                    <i>${item.date.toLocaleDateString()}</i>
                 </div>
             </li>`);
+
+        if (dialog.querySelector('.mark') == null) {
+            dialog.lastChild.scrollIntoView({ block: "end" });
+        } else {
+            dialog.querySelector('.mark').scrollIntoView({ block: "end", behavior: "smooth" });
+        }
     });
-    dialog.insertAdjacentHTML('beforeend', `<div id='anchor${index}'>`);
+
 
     const sendForm = document.getElementById('sendForm');
     sendForm.innerText = '';
@@ -180,42 +255,53 @@ function renderDialog(a, index) {
     const sendBut = document.getElementById('sendBut');
     const inputText = document.getElementById('inputText');
 
-    sendBut.addEventListener('click', function() { sendMessage(a, index) });
+    sendBut.addEventListener('click', function() {
+        sendMessage(a)
+    });
     inputText.addEventListener('keydown', function() {
         if (event.keyCode == 13) {
-            sendMessage(a, index);
+            sendMessage(a);
         }
     });
 };
 
-const sendMessage = function(a, index) {
+const sendMessage = function(a) {
     if (inputText.innerText !== "") {
         a.messages.push({
             date: new Date(),
             name: 'You',
             text: inputText.innerText
         })
-        renderDialog(a, index);
+        renderDialog(a);
         renderDialogsList();
-        location = `#anchor${index}`;
     }
 };
 
 function renderDialogsList() {
     dialogsList.innerText = '';
+    let lastMessage = {};
 
     dialogs.forEach((item, index) => {
-
-        let lastMessage = item.messages.slice().pop();
+        lastMessage = item.messages.slice().pop();
         for (let i = 0; i < item.messages.length; i++) {
             if (item.messages[i].mark == 1) {
                 lastMessage = item.messages[i];
             }
         }
-
-        dialogsList.insertAdjacentHTML('beforeend', `
+        if (lastMessage == undefined) {
+            dialogsList.insertAdjacentHTML('beforeend', `
+            <li class='my-dialogs-list-item' id=dialod#${index}>
+                <figure>
+                    <figcaption>${item.dialogName}</figcaption>
+                    <img alt="ava" src='${item.avatar}' class='my-dialogs-ava'>
+                </figure>
+                <div class='my-dialogs-info'>
+                    <p>Сообщений нет</p>
+                </div>
+            </li>`);
+        } else {
+            dialogsList.insertAdjacentHTML('beforeend', `
         <li class='my-dialogs-list-item' id=dialod#${index}>
-            <a href='#anchor${index}'>
             <figure>
                 <figcaption>${item.dialogName}</figcaption>
                 <img alt="ava" src='${item.avatar}' class='my-dialogs-ava'>
@@ -224,16 +310,18 @@ function renderDialogsList() {
                 <span>${lastMessage.date.toLocaleString()}</span>
                 <p>${lastMessage.name}:<br/> ${(lastMessage.text.length<35) ? lastMessage.text : lastMessage.text.substr(0,35)+'...'}</p>
             </div>
-            </a>
         </li>`);
+        }
 
         const d = document.getElementById(`dialod#${index}`);
-        d.addEventListener('click', function() { renderDialog(item, index) });
+        d.addEventListener('click', function() { renderDialog(item); });
 
         if (item.display == 'hidden') {
             d.classList.add('hidden');
         }
 
+
     })
 };
+
 renderDialogsList();
